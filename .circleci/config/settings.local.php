d7fe0d22c8b52aa5e51722ad99a3cd2912ef5064
Juampy <juampynr@gmail.com>
2019-08-07 06:41:23 +0200

    Add CircleCI initial implementation

---
+++ .circleci/config/settings.local.php
@@ -0,0 +1,27 @@
+<?php
+
+$databases['default']['default'] = [
+  'database' => 'drupal8_config',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+
+// The Drupal 7 database connection details.
+$databases['drupal7']['default'] = [
+  'database' => 'drupal7',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+
+// Display errors.
+$config['system.logging']['error_level'] = 'verbose';
